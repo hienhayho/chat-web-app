5a3b98697a90a174ff8123286912a59db3517f22
hienhayho <115549171+hienhayho@users.noreply.github.com>
2024-02-17 09:12:19 +0700

    add alias @

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  }
 })
